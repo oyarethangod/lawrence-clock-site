646b0a00aca71e0e7c86335b712cc6d16878bcc9
oyarethangod <oyarethankgod@gmail.com>
2024-11-23 16:16:14 +0100

    clock website

---
+++ script.js
@@ -0,0 +1,18 @@
+const hour = document.querySelector('.hour')
+const minute = document.querySelector('.minute')
+const second = document.querySelector('.second')
+
+
+
+
+setInterval(() => {
+    const date = new Date
+    const hourDegree = date.getHours()/12 * 360 - 90
+    const minuteDegree = date.getMinutes()/60 * 360 - 90
+    const secondDegree = date.getSeconds()/60 * 360 - 90
+
+    hour.style.transform = `rotate(${hourDegree}deg)`
+    minute.style.transform = `rotate(${minuteDegree}deg)`
+    second.style.transform = `rotate(${secondDegree}deg)`
+}, 1000);
+
